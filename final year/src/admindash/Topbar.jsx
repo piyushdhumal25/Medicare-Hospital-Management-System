064d5c42eff0e607f5c9659bed5ce5692b12a5cf
ingoleanushka20 <ingoleanushka20@gmail.com>
2025-08-17 23:17:11 +0530

    css part

--- final year/src/admindash/Topbar.jsx
+++ final year/src/admindash/Topbar.jsx
@@ -3,19 +3,11 @@ import { LogOut } from 'lucide-react';
 
 const Topbar = () => {
   return (
-    <div className="bg-white shadow-lg rounded-b-xl px-8 py-4 flex justify-between items-center border-b border-gray-100">
-      {/* Left side: Branding */}
-      
-
-      {/* Right side: Profile + Logout */}
-      <div className="flex items-center gap-4">
-        {/* Profile Initials Avatar */}
-        <div className="w-10 h-10 rounded-full bg-blue-100 text-blue-700 flex items-center justify-center font-bold text-sm shadow-inner">
-      
+    <div className="bg-white ">
+     <div className="w-10 h-10 ">
+       </div>
         </div>
-
-      </div>
-    </div>
+    
   );
 };
 
